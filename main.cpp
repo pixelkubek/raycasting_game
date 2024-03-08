1b099f4f16c0ebb9378ceae4ff49d0c2fc11797a
pixelkubek <czyszczon.kuba@gmail.com>
2024-03-08 18:33:49 +0100

    Start of ray casting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
 #include <iostream>
 #include <random>
-#include <cmath>
 #include <vector>
 
 class Window {
@@ -9,7 +9,11 @@ class Window {
     size_t LENGTH, HEIGHT;
     sf::VertexArray pixels;
 
-    public:
+    bool isEqualFloat(float a, float b) {
+        return fabsf(a - b) < 10e-10;
+    }
+
+public:
     Window(size_t length, size_t height, std::string title) {
         LENGTH = length;
         HEIGHT = height;
@@ -27,12 +31,31 @@ class Window {
         return pRenderWindow->isOpen();
     }
 
-    void setPixelColor(size_t x, size_t y, sf::Color color) {
-        pixels[y * LENGTH + x].color = color;
+    void setPixelColor(int x, int y, sf::Color color) {
+        if (x < 0 || (size_t)x >= LENGTH || y < 0 || (size_t)y >= HEIGHT) return;
+        pixels[(HEIGHT - y) * LENGTH + x].color = color;
     }
 
-    void drawLine(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, sf::Color color){
-        
+    void drawLine(float x1, float y1, float x2, float y2, sf::Color color) {
+        float incriment, x, y;
+
+        if (!isEqualFloat(x1, x2)) {
+            incriment = (y2 - y1) / (x2 - x1);
+            y = y1;
+            for (x = x1; x <= x2; x++) {
+                setPixelColor((int)x, (int)y, color);
+                y += incriment;
+            }
+        }
+
+        if (!isEqualFloat(y1, y2)) {
+            x = x1;
+            incriment = (x2 - x1) / (y2 - y1);
+            for (y = y1; y < y2; y++) {
+                setPixelColor((int)x, (int)y, color);
+                x += incriment;
+            }
+        }
     }
 
     void display() {
@@ -58,6 +81,11 @@ class Player {
     float x, y, angle;
 
     public:
+    float getX() {return x;}
+    float getY() {return y;}
+    float getAngle() {return angle;}
+
+public:
     Player(float start_x, float start_y, float start_angle) {
         x = start_x;
         y = start_y;
@@ -69,6 +97,7 @@ class Game {
     Window* pWindow;
     size_t LENGTH, HEIGHT;
     unsigned int renderDelayMs = 30;
+    int fov = 60;
     Player* pPlayer;
     unsigned int rayCastingPrecision = 64;
     std::vector<std::vector<int>> map;
@@ -77,29 +106,38 @@ class Game {
         return degrees * (float)M_PI / 180;
     }
 
-    public:
+public:
     Game(size_t length, size_t height) {
         LENGTH = length;
         HEIGHT = height;
         pWindow = new Window(LENGTH, HEIGHT, "test");
 
         map = {
-            {1,1,1,1,1,1,1,1,1,1},
-            {1,0,0,0,0,0,0,0,0,1},
-            {1,0,0,0,0,0,0,0,0,1},
-            {1,0,0,1,1,0,1,0,0,1},
-            {1,0,0,1,0,0,1,0,0,1},
-            {1,0,0,1,0,0,1,0,0,1},
-            {1,0,0,1,0,1,1,0,0,1},
-            {1,0,0,0,0,0,0,0,0,1},
-            {1,0,0,0,0,0,0,0,0,1},
-            {1,1,1,1,1,1,1,1,1,1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 1, 1, 0, 1, 0, 0, 1},
+            {1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+            {1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+            {1, 0, 0, 1, 0, 1, 1, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
         };
     }
 
+    void rayCasting() {
+        float rayAngle = pPlayer->getAngle() - (float)fov / 2.f;
+        float incrimentAngle = (float)fov / (float)LENGTH;
+        for(unsigned int rayCount = 0; rayCount < LENGTH; rayCount++) {
+
+            rayAngle += incrimentAngle;
+        }
+    }
+
     void play() {
-        while(pWindow->isOpen()){
-            pWindow->setPixelColor(100, 100, sf::Color(rand()));
+        while (pWindow->isOpen()) {
+            pWindow->drawLine(100, 100, 100, 200, sf::Color::Black);
             pWindow->display();
         }
     }
@@ -114,8 +152,6 @@ int main() {
 
     Game game(LENGTH, HEIGHT);
     game.play();
-
-
 
     return 0;
 }
