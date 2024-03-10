a1a1185405b2a65fae72ca24d26c8bcc596cc784
pixelkubek <czyszczon.kuba@gmail.com>
2024-03-10 15:07:13 +0100

    Separate player class file

--- src/main.cpp
+++ src/main.cpp
@@ -10,86 +10,11 @@
 #include "texture.h"
 #include "map.h"
 #include "window.h"
+#include "player.h"
 
 float degreesToRadians(float degrees) {
     return degrees * (float)M_PI / 180.f;
 }
-
-// Class representing a player and his movement.
-class Player {
-    float x, y, angle, speed = 0.005f, horizontalSpeed = 0.002f, angularSpeed = 0.18f;
-
-public:
-    Player() {
-        // Maze generation algorithm ensures these coordinates are free
-        x = 1.5f;
-        y = 1.5f;
-        angle = 0.f;
-    }
-
-    float getX() { return x; }
-    void setX(float newX) { x = newX; }
-    float getY() { return y; }
-    void setY(float newY) { y = newY; }
-    float getAngle() { return angle; }
-
-    Player(float start_x, float start_y, float start_angle) {
-        x = start_x;
-        y = start_y;
-        angle = start_angle;
-    }
-
-private:
-    // move forward and right relative to current position and angle
-    void moveRelative(float forwardDistance, float rightDistance, const std::vector<std::vector<int>> &map) {
-        float new_x = x, new_y = y;
-        new_x += forwardDistance * sinf(degreesToRadians(angle));
-        new_y += forwardDistance * cosf(degreesToRadians(angle));
-        new_x += rightDistance * sinf(degreesToRadians(angle + 90));
-        new_y += rightDistance * cosf(degreesToRadians(angle + 90));
-
-        // Check for collision.
-        if (!map[(int)new_y][(int)new_x]) {
-            x = new_x;
-            y = new_y;
-        } else if (!map[(int)y][(int)new_x]) { // Slide on walls instead of stopping
-            x = new_x;
-        } else if (!map[(int)new_y][(int)x]) {
-            y = new_y;
-        }
-    }
-
-public:
-    void movement(float deltaTime, const std::vector<std::vector<int>> &map) {
-        // std::cout << angle << "\n";
-        // DeltaTime ensures similar real time player speed between different framerates.
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            moveRelative(speed * deltaTime, 0, map);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            moveRelative(-speed * deltaTime, 0, map);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::LAlt)) {
-                moveRelative(0, -horizontalSpeed * deltaTime, map);
-            } else {
-                angle -= angularSpeed * deltaTime;
-            }
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::LAlt)) {
-                moveRelative(0, horizontalSpeed * deltaTime, map);
-            } else {
-                angle += angularSpeed * deltaTime;
-            }
-        }
-
-        while (angle > 360)
-            angle -= 360;
-        while (angle < 0)
-            angle += 360;
-    }
-};
 
 // Class representing game logic.
 class Game {
---
+++ src/player.h
@@ -0,0 +1,29 @@
+#ifndef playerH
+#define playerH
+
+#include <vector>
+
+// Class representing a player and his movement.
+class Player {
+    float x, y, angle, speed = 0.005f, horizontalSpeed = 0.002f, angularSpeed = 0.18f;
+
+public:
+    Player();
+
+    float getX();
+    void setX(float newX);
+    float getY();
+    void setY(float newY);
+    float getAngle();
+
+    Player(float start_x, float start_y, float start_angle);
+
+private:
+    // move forward and right relative to current position and angle
+    void moveRelative(float forwardDistance, float rightDistance, const std::vector<std::vector<int>> &map);
+
+public:
+    void movement(float deltaTime, const std::vector<std::vector<int>> &map);
+};
+
+#endif
